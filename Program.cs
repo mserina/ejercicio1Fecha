dd1d743d0e2238888cbdce39109737d89f76f22e
DESKTOP-PR2BK7P\csi23-mserina <csi23-mserina@colegioaltair.net>
2024-02-02 13:57:40 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace ejercicio1Fecha
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //CREACION
+            DateTime fechaHoy = DateTime.Now;
+
+            //Para sacar todo los formatos de la cultura española que contengan la palabra "Pattern"
+            DateTimeFormatInfo infoFormatoCulturaEspecifica = CultureInfo.GetCultureInfo("es-SP").DateTimeFormat;
+            Type tipo = infoFormatoCulturaEspecifica.GetType();
+            PropertyInfo[] propiedadesFormatos = tipo.GetProperties();
+            foreach (var propiedad in propiedadesFormatos)
+            {
+                if (propiedad.Name.Contains("Pattern"))
+                {
+                    string formato = propiedad.GetValue(infoFormatoCulturaEspecifica, null).ToString();
+                    Console.WriteLine(propiedad.Name + ": " + formato + " " +
+                                      fechaHoy.ToString(formato));
+                }
+            }
+            //Imprimir la fecha que hemos puesto al principio con un formato concreto
+            Console.WriteLine(" ");
+            Console.WriteLine("Formato fecha-tiempo: " + fechaHoy.ToString("s"));
+
+
+        }  
+    }
+}
